1e4f28a571d71d298fe5572a2d06eae1d581b3c8
Loris Birkemeyer <loris.birkemeyer@jeunes-europeens.org>
2022-02-07 16:44:20 +0100

    feat: add meta tags

--- components/layout/TemplateBase.js
+++ components/layout/TemplateBase.js
@@ -2,9 +2,12 @@ import {useTranslations} from 'next-intl';
 import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
+import {useRouter} from 'next/router';
 
 export default function TemplateBase({children, title}) {
 	const t = useTranslations('PageLayout');
+	const {locale, locales} = useRouter()
+	const otherLocales = locales?.filter((cur) => cur !== locale && cur !== 'default');
 
 	const joinedTitle =
 		title !== t('siteTitle')
@@ -15,8 +18,30 @@ export default function TemplateBase({children, title}) {
 		<>
 			<Head>
 				<title>{joinedTitle}</title>
-				<meta name="description" content={t('siteDescription')} />
 				<link rel="icon" href="/favicon.ico?v=2" />
+
+				{/*Primary Meta Tags*/}
+				<meta name="title" content={t('siteTitle')} />
+				<meta name="description" content={t('siteDescription')} />
+
+				{/*Open Graph / Facebook*/}
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://www.strasbourgsummit.eu/" />
+				<meta property="og:title" content={t('siteTitle')} />
+				<meta property="og:description" content={t('siteDescription')} />
+				<meta property="og:image" content={`https://www.strasbourgsummit.eu/OG-Sommet-${locale}.png`} />
+				<meta property="og:locale" content={locale} />
+				{otherLocales.map((otherLocale) => (
+					<meta key={otherLocale} property="og:locale:alternate" content={otherLocale} />
+				))}
+xxx
+				{/*Twitter*/}
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:domain" content="strasbourgsummit.eu" />
+				<meta property="twitter:url" content="https://www.strasbourgsummit.eu/" />
+				<meta property="twitter:title" content={t('siteTitle')} />
+				<meta property="twitter:description" content={t('siteDescription')} />
+				<meta property="twitter:image" content={`https://www.strasbourgsummit.eu/OG-Sommet-${locale}.png`} />
 			</Head>
 
 			<Header />
