006477c22f781f081bbc317a6207f97f517a52b2
Loris Birkemeyer <loris.birkemeyer@jeunes-europeens.org>
2022-02-07 17:21:11 +0100

    feat: add matomo

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
+import App from "next/app";
 import {NextIntlProvider} from 'next-intl'
+
+import { init } from "@socialgouv/matomo-next";
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <NextIntlProvider messages={pageProps.messages}>
-      <Component {...pageProps} />
-    </NextIntlProvider>
-  )
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
+
+class MyApp extends App {
+  componentDidMount() {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <NextIntlProvider messages={pageProps.messages}>
+        <Component {...pageProps} />
+      </NextIntlProvider>
+    )
+  }
 }
 
 export default MyApp
