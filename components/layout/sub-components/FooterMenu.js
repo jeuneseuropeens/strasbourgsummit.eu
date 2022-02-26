192cbde6378ffb29510afbcdd5c985d7507dd202
Loris Birkemeyer <loris.birkemeyer@jeunes-europeens.org>
2022-02-26 15:04:52 +0100

    fix(footer): open social media in new tab

--- components/layout/sub-components/FooterMenu.js
+++ components/layout/sub-components/FooterMenu.js
@@ -67,8 +67,15 @@ export default function FooterMenu() {
 
 			<div className="mt-8 flex justify-center space-x-6">
 				{navigation.social.map((item) => (
-					<a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500" title={item.name}>
-						<span className="sr-only">{item.name}</span>
+					<a
+						key={item.name}
+						href={item.href}
+						className="text-gray-400 hover:text-gray-500"
+						title={item.name} target="blank"
+					>
+						<span className="sr-only">
+							{item.name}
+						</span>
 						<item.icon className="h-6 w-6" aria-hidden="true" />
 					</a>
 				))}
