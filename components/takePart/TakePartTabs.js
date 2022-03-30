558c758e703c809ccb41db4d459f242e984536d2
Loris Birkemeyer <loris.birkemeyer@jeunes-europeens.org>
2022-03-30 22:01:33 +0200

    feat(take-part): add margin

--- components/takePart/TakePartTabs.js
+++ components/takePart/TakePartTabs.js
@@ -29,7 +29,7 @@ export default function TakePartTabs() {
 				tabPanels={categories}
 				tabContent={
 					categories.map((category) => (
-						<TabPanel key={category.id} className="mt-8 sm:mt-12 lg:max-w-4xl lg:mx-auto">
+						<TabPanel key={category.id} className="mt-12 sm:mt-16 lg:max-w-4xl lg:mx-auto">
 							{category.id === 'organisation' && <TakePartOrganisation/>}
 							{category.id === 'individual' && <TakePartIndividual/>}
 						</TabPanel>
