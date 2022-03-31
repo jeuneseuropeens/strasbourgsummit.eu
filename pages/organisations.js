b5635f5b24b4118fd05828e96340254a01cefb26
Loris Birkemeyer <loris.birkemeyer@jeunes-europeens.org>
2022-03-31 15:25:31 +0200

    feat: add logos

--- pages/organisations.js
+++ pages/organisations.js
@@ -11,7 +11,7 @@ function LeadingOrganisations() {
 			website: 'https://www.jeunes-europeens.org/',
 		},
 		{
-			name: 'Union des Fédéralistes Européens',
+			name: 'Union des Fédéralistes Européens - France',
 			image: 'https://www.uef.fr/IMG/siteon0.jpg?1458078215',
 			website: 'https://www.uef.fr/',
 			maxHeight: 'max-h-16',
@@ -52,6 +52,31 @@ function LeadingOrganisations() {
 			name: 'European Youth Forum',
 			image: '/YFJ-logo-blue-regular.png',
 			website: 'https://www.youthforum.org/'
+		},
+		{
+			name: 'Civil Society Europe',
+			image: 'https://civilsocietyeurope.eu/wp-content/uploads/2019/02/Logo-transparent.png',
+			website: 'https://civilsocietyeurope.eu'
+		},
+		{
+			name: 'Junge Europäische Föderalisten (JEF) Deutschland',
+			image: 'https://www.jef.de/wp-content/uploads/sites/4/2020/04/Logo_RGBBV.png',
+			website: 'https://www.jef.de'
+		},
+		{
+			name: 'Gioventù Federalista Europea (GFE)',
+			image: 'https://giovanifederalisti.eu/wp-content/uploads/2020/03/GFE-Logo-w512.png',
+			website: 'https://giovanifederalisti.eu'
+		},
+		{
+			name: 'Europa-Union Saar',
+			image: '/eu-saar.svg',
+			website: 'https://www.eu-saar.de'
+		},
+		{
+			name: 'Europa-Union Baden-Württemberg',
+			image: 'https://scontent.fcdg2-1.fna.fbcdn.net/v/t1.6435-9/104099809_3052845964793228_1788247787225404219_n.png?_nc_cat=106&ccb=1-5&_nc_sid=dd9801&efg=eyJpIjoidCJ9&_nc_ohc=gA64AyXTPA4AX9ID9l7&tn=3O8MUq65fTGnEE3d&_nc_ht=scontent.fcdg2-1.fna&oh=00_AT-1i4tpkpgubhCbZZO-i04B1x3OP5O9JlcLW8pEPp5-2w&oe=626B85D2',
+			website: 'https://eubw.eu/'
 		}
 	]
 
@@ -60,21 +85,21 @@ function LeadingOrganisations() {
 			{/*<p className="text-center text-base font-semibold uppercase text-gray-600 tracking-wider">*/}
 			{/*	{t('projectLedBy')}*/}
 			{/*</p>*/}
-			<div className="mt-6 grid sm:grid-cols-2 gap-2 md:grid-cols-3 lg:mt-8">
+			<div className="mt-6 grid sm:grid-cols-2 gap-4 md:grid-cols-3 lg:mt-8">
 				{organisations.map((partner) => (
 					<a
 						key={partner.name}
 						href={partner.website}
 						target="_blank"
 						rel="noreferrer"
-						className="block col-span-1 flex justify-center items-center py-8 px-8 bg-white"
+						className="transition duration-500 block col-span-1 flex flex-col justify-center items-center p-8 bg-white rounded-lg drop-shadow-none hover:drop-shadow-lg"
 					>
 						<img
 							className={partner.maxHeight || 'max-h-20'}
 							src={partner.image}
 							alt={partner.name}
 						/>
-						{/*<p>{partner.website}</p>*/}
+						<p className="text-center mt-2 text-gray-600">{partner.name}</p>
 					</a>
 				))}
 			</div>
