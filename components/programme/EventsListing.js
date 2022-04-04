d13077bc914efce4f3fe3a3b00840ac667672cde
Loris Birkemeyer <loris.birkemeyer@jeunes-europeens.org>
2022-04-05 01:50:39 +0200

    debug: use ErrorBoundary

--- components/programme/EventsListing.js
+++ components/programme/EventsListing.js
@@ -1,17 +1,20 @@
 import EventPreview from './EventPreview'
+import ErrorBoundary from '../commons/ErrorBoundary'
 
 export default function EventsListing({ events, umbrellas }) {
 	return (
-		<section>
-			<ul className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3 pt-16 pb-32">
-				{events.map((event) => event.title && (
-					<EventPreview
-						key={event.slug}
-						event={event}
-						umbrellas={umbrellas}
-					/>
-				))}
-			</ul>
-		</section>
+		<ErrorBoundary>
+			<section>
+				<ul className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3 pt-16 pb-32">
+					{events.map((event, index) => event.title && (
+						<EventPreview
+							key={index}
+							event={event}
+							umbrellas={umbrellas}
+						/>
+					))}
+				</ul>
+			</section>
+		</ErrorBoundary>
 	)
 }
