d6040469b75bf27ed176ce1e22752df39ecc9fbb
Loris Birkemeyer <loris.birkemeyer@jeunes-europeens.org>
2022-04-19 15:09:40 +0200

    feat: update saturday programme

--- components/programme/saturday/ProgrammeSaturday.js
+++ components/programme/saturday/ProgrammeSaturday.js
@@ -1,55 +1,67 @@
 import {useTranslations} from 'next-intl'
-import {ChevronDoubleRightIcon, LocationMarkerIcon} from '@heroicons/react/solid'
+import {ChatAlt2Icon, ChevronDoubleRightIcon, LocationMarkerIcon, SpeakerphoneIcon} from '@heroicons/react/solid'
 import StaggeredElement from './StaggeredElement'
+import SingleFeedItem from '../../commons/feed/SingleFeedItem'
+import FeedListing from '../../commons/feed/FeedListing'
 
 export default function ProgrammeSaturday () {
 	const t = useTranslations('pages.Programme.events.saturday')
 
-	const events = [
-		{
-			time: t('convention.time'),
-			title: t('convention.title'),
-			description: t('convention.description'),
-			imageSrc:'/aubette.jpg',
-			// timeline: [
-			// 	{
-			// 		content: t('convention.timeline.location.content'),
-			// 		target: t('convention.timeline.location.target'),
-			// 		icon: LocationMarkerIcon,
-			// 		iconBackground: 'bg-primary-400',
-			// 	},
-			// ],
-		},
-		{
-			time: t('march.time'),
-			title: t('march.title'),
-			description: t('march.description'),
-			imageSrc:'/marche.jpg',
-			timeline: [
-				{
-					before: t('march.timeline.location.content'),
-					main: t('march.timeline.location.target'),
-					icon: LocationMarkerIcon,
-					iconBackground: 'bg-primary-400',
-				},
-				{
-					before: t('march.timeline.destination.content'),
-					main: t('march.timeline.destination.target'),
-					icon: ChevronDoubleRightIcon,
-					iconBackground: 'bg-primary-300',
-				},
-			],
-		},
-	]
-
 	return (
 		<div className="relative pt-16 pb-32">
-			{events.map((event, index) => (
-				<StaggeredElement
-					key={index}
-					event={event}
-				/>
-			))}
+			<StaggeredElement
+				title={t('convention.title')}
+				description={t('convention.description')}
+				imageSrc={'/aubette.jpg'}
+			>
+				<SingleFeedItem item={{
+					main: t('convention.location.main'),
+					icon: LocationMarkerIcon,
+					href: '/map',
+					iconBackground: 'bg-primary-400',
+				}}/>
+
+				<div className="mt-4">
+					<SingleFeedItem item={{
+						main: "09:00",
+						after: " - 12:30",
+						icon: ChatAlt2Icon,
+						iconBackground: 'bg-primary-400',
+					}}/>
+					<p className="mt-2">{t('convention.part1')}</p>
+				</div>
+
+				<div className="mt-4">
+					<SingleFeedItem item={{
+						main: "14:30",
+						after: " - 15:30",
+						icon: SpeakerphoneIcon,
+						iconBackground: 'bg-primary-400',
+					}}/>
+					<p className="mt-2">{t('convention.part2')}</p>
+				</div>
+			</StaggeredElement>
+
+			<StaggeredElement
+				title={t('march.title')}
+				description={t('march.description')}
+				imageSrc={'/marche.jpg'}
+			>
+				<FeedListing timeline={[
+					{
+						before: t('march.timeline.location.before'),
+						main: t('march.timeline.location.main'),
+						icon: LocationMarkerIcon,
+						iconBackground: 'bg-primary-400',
+					},
+					{
+						before: t('march.timeline.destination.before'),
+						main: t('march.timeline.destination.main'),
+						icon: ChevronDoubleRightIcon,
+						iconBackground: 'bg-primary-300',
+					}
+				]}/>
+			</StaggeredElement>
 		</div>
 	)
 }
--- components/programme/saturday/StaggeredElement.js
+++ components/programme/saturday/StaggeredElement.js
@@ -1,34 +1,27 @@
-import FeedListing from '../../commons/feed/FeedListing'
 import {classNames} from '../../../lib/helpers'
 import {Badge} from '../../commons/Badge'
 import Title from '../../commons/Title'
 
-export default function StaggeredElement ({event}) {
+export default function StaggeredElement ({time, title, description, imageSrc, imageAlt, timeline, children}) {
 	return (
 		<div className="group relative mt-24 first:mt-0">
 			<div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
 				<div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 group-even:lg:col-start-2">
-					<div>
-						{event.time && (
-							<Badge>{event.time}</Badge>
-						)}
-					</div>
+					{time && (
+						<Badge>{time}</Badge>
+					)}
 
 					<div className="mt-6">
 						<Title tag='h2' extrabold>
-							{event.title}
+							{title}
 						</Title>
 
-						<p className="mt-4 text-lg text-gray-500">
-							{event.description}
+						<p className="mt-4 text-lg text-gray-500 whitespace-pre-line">
+							{description}
 						</p>
 					</div>
 
-					{event.timeline && (
-						<div className="mt-6">
-							<FeedListing timeline={event.timeline}/>
-						</div>
-					)}
+					<div className="mt-6">{children}</div>
 				</div>
 
 				<div className="mt-12 sm:mt-16 lg:mt-0 group-even:lg:col-start-1">
@@ -42,8 +35,8 @@ export default function StaggeredElement ({event}) {
 								'group-even:lg:right-0 group-odd:lg:left-0 w-full',
 								'rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:h-full lg:w-auto lg:max-w-none'
 							)}
-							src={event.imageSrc}
-							alt={event.imageAlt}
+							src={imageSrc}
+							alt={imageAlt}
 						/>
 					</div>
 				</div>
